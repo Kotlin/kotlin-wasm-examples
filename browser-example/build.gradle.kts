b5db30dc5afcb365963ca7b0e4b29622da2bfd19
Sébastien Deleuze <sdeleuze@vmware.com>
2023-02-09 09:00:42 +0100

    Remove unneeded repositories

--- browser-example/build.gradle.kts
+++ browser-example/build.gradle.kts
@@ -8,7 +8,6 @@ group = "me.user"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     mavenCentral()
 }
 
--- browser-example/settings.gradle.kts
+++ browser-example/settings.gradle.kts
@@ -1,9 +1,7 @@
-
 rootProject.name = "kotlin-wasm-browser-example"
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
         gradlePluginPortal()
     }
 }
