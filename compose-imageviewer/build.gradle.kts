606d6c17e1a5973098e6ea1f8a2440aaed02f73c
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-04-12 20:23:53 +0200

    Fix android build
    by oleksandr.karpovich

--- compose-imageviewer/build.gradle.kts
+++ compose-imageviewer/build.gradle.kts
@@ -17,4 +17,18 @@ allprojects {
         mavenLocal()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
     }
+
+    configurations.all {
+        val conf = this
+        // Currently it's necessary to make the android build work properly
+        conf.resolutionStrategy.eachDependency {
+            val isWasm = conf.name.contains("wasm", true)
+            val isJs = conf.name.contains("js", true)
+            val isComposeGroup = requested.module.group.startsWith("org.jetbrains.compose")
+            val isComposeCompiler = requested.module.group.startsWith("org.jetbrains.compose.compiler")
+            if (isComposeGroup && !isComposeCompiler && !isWasm && !isJs) {
+                useVersion("1.3.1")
+            }
+        }
+    }
 }
