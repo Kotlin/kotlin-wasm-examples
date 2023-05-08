c2c79bcdc81ed1919b52b16319e509c9518e8b08
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2023-05-08 17:10:50 +0200

    Jetsnack: and a message with an instruction on how to enable required features in the browser
    Also, add an overlay message with some useful links.

--- compose-jetsnack/web/src/wasmMain/resources/load.mjs
+++ compose-jetsnack/web/src/wasmMain/resources/load.mjs
@@ -1,4 +1,16 @@
 import { instantiate } from './jetsnackwasmapp.uninstantiated.mjs';
 
 await wasmSetup;
-instantiate({ skia: Module['asm'] });
+
+let te = null;
+try {
+    await instantiate({ skia: Module['asm'] });
+} catch (e) {
+  te = e;  
+}
+
+if (te == null) {
+    document.getElementById("warning").style.display="none";
+} else {
+    throw te;
+}
