d28f7917848c24e26d40f8ec6b309dcd9ba0e77f
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2023-05-12 13:02:23 +0200

    Jetsnack: use a proper version of webpack

--- compose-jetsnack/web/build.gradle.kts
+++ compose-jetsnack/web/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.compose.compose
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
@@ -53,3 +54,6 @@ kotlin {
 compose.experimental {
     web.application {}
 }
+
+// Use a proper version of webpack, TODO remove after updating to Kotlin 1.9. 
+rootProject.the<NodeJsRootExtension>().versions.webpack.version = "5.76.2"
