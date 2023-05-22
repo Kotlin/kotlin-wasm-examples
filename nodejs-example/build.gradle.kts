a0f93f20ac1651ecc7d0ada304b3840359cb1af3
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2023-05-23 01:26:04 +0200

    Node.js example: use node.js 20.2.0

--- nodejs-example/build.gradle.kts
+++ nodejs-example/build.gradle.kts
@@ -18,8 +18,7 @@ kotlin {
 }
 
 rootProject.the<NodeJsRootExtension>().apply {
-    nodeVersion = "20.0.0-v8-canary2023022187dda913b0"
-    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+    nodeVersion = "20.2.0"
 }
 
 // Use a proper version of webpack, TODO remove after updating to Kotlin 1.9.
