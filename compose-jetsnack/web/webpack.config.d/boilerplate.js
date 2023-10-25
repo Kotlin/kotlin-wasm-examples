aded9e72c67b9d7dc38ce6404c12e795f485f642
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2023-10-25 20:25:25 +0200

    Use another way to workaround unresolved skia module in webpack

--- compose-jetsnack/web/webpack.config.d/boilerplate.js
+++ compose-jetsnack/web/webpack.config.d/boilerplate.js
@@ -2,17 +2,6 @@ config.entry = {
     main: [require('path').resolve(__dirname, "kotlin/load.mjs")]
 };
 
-class IgnoreImportErrorsPlugin {
-    apply(compiler) {
-        compiler.hooks.done.tap('IgnoreImportErrorsPlugin', (stats) => {
-            stats.compilation.errors = stats.compilation.errors.filter((error) => {
-                if (error.message.includes("skia")) {
-                    return false; // Remove the error
-                }
-                return true; // Keep the error
-            });
-        });
-    }
-}
-
-config.plugins.push(new IgnoreImportErrorsPlugin());
+config.resolve ?? (config.resolve = {});
+config.resolve.alias ?? (config.resolve.alias = {});
+config.resolve.alias.skia = false;
