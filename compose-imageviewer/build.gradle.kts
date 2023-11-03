6fc3096c1f5ffa3b2f8ac5fbd22f97cda5e39baa
oleksandr.karpovich <a.n.karpovich@gmail.com>
2023-11-03 15:03:15 +0100

    Remove outdated workaround

--- compose-imageviewer/build.gradle.kts
+++ compose-imageviewer/build.gradle.kts
@@ -21,23 +21,6 @@ allprojects {
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
 
-    configurations.all {
-        val conf = this
-        // Currently it's necessary to make the android build work properly
-        conf.resolutionStrategy.eachDependency {
-            if (requested.module.name.startsWith("kotlin-stdlib")) {
-                val kotlinVersion = project.property("kotlin.version") as String
-                useVersion(kotlinVersion)
-            }
-            if (requested.module.name.contains("kotlinx-serialization") &&
-                // kotlinx-datetime-wasm-js:0.4.1-wasm0 depends on outdated kotlinx-serialization-core:1.5.2-wasm0
-                requested.version == "1.5.2-wasm0"
-            ) {
-                useVersion("1.6.1-wasm0")
-            }
-        }
-    }
-
     afterEvaluate {
         extensions.findByType(ComposeExtension::class.java)?.apply {
             val composeCompilerVersion = project.property("compose.compiler.version") as String
