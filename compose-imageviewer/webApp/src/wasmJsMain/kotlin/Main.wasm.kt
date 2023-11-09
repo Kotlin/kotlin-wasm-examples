7f74c56e741d84f70508489d53e7151e112dc8bd
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2023-11-09 16:44:29 +0100

    Use Compose 1.5.10-dev-wasm03 with new resources feature

--- compose-imageviewer/webApp/src/wasmJsMain/kotlin/Main.wasm.kt
+++ compose-imageviewer/webApp/src/wasmJsMain/kotlin/Main.wasm.kt
@@ -3,10 +3,17 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import example.imageviewer.*
 import example.imageviewer.model.*
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.configureWebResources
+import org.jetbrains.compose.resources.urlResource
 import kotlin.wasm.unsafe.*
 
-@OptIn(ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalResourceApi::class)
 fun main() {
+    configureWebResources {
+        // same as default - this is not necessary to add here. It's here to show this feature
+        setResourceFactory { urlResource("./$it") }
+    }
     CanvasBasedWindow("ImageViewer") {
         ImageViewerWeb()
     }
