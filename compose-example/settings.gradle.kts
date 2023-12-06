e43e6ee932d5dd9ab30c597414237969d07428c8
oleksandr.karpovich <a.n.karpovich@gmail.com>
2023-12-06 15:04:47 +0100

    Remove dev maven repo

--- compose-example/settings.gradle.kts
+++ compose-example/settings.gradle.kts
@@ -3,7 +3,6 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
         mavenCentral()
@@ -14,7 +13,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
 
